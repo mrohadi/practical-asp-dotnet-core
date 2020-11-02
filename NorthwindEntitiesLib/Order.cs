b9afce757bc8a834a4c04af250c89fd3ed5e32d3
Muhammad Rohadi <mrohadi@Muhammads-MacBook-Air.local>
2020-11-02 14:19:50 +0700

    adding  northwind  database

---
+++ NorthwindEntitiesLib/Order.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace Packt.Shared
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public int EmployeeID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }  
+        public int ShipVia { get; set; }
+        public decimal? Freight { get; set; } = 0;
+
+        // related entities
+        public Customer Customer { get; set; }
+        public Employee Employee { get; set; }
+        public Shipper Shipper { get; set; }
+
+        // Related Entities
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+}
